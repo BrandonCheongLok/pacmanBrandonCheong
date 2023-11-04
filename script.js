21b4d5f2188cc9658625bbcd6a086690710f048f
BrandonCheongLok <brandoncheong@bricks.id>
2023-11-04 15:30:45 +0800

    hjh

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 
-canvas.width = innerWidth;
 canvas.height = innerHeight;
 const fps = 100;
 const blockSize = 50;
-const startPosX = 20;
-const startPosY = 20;
+const startPosX = 0;
+const startPosY = 0;
 const boundaries = [];
-
+const foods = [];
 const map = [['1', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '2'],
              ['|', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '|'],
              ['|', '.', '.', '[', '-', '-', ']', '.', '.', '.', '.', '.', '.', '.', '[', ']', '.', '|'],
@@ -19,7 +18,8 @@ const map = [['1', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '
              ['|', '.', '.', '.', '.', 'b', '.', '.', '.', '.', '.', '.', '_', '.', '.', '.', '.', '|'],
              ['|', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '|'],
              ['3', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '4']]
-
+canvas.width = blockSize*map[0].length;
+canvas.height = blockSize*map.length;
 const textToImg = {
     '-' : './images/pipeHorizontal.png',
     '|' : './images/pipeVertical.png',
@@ -43,9 +43,11 @@ const textToImg = {
 class Player{
     constructor(pos){
         this.pos = pos
-        this.radius = 40
+        this.radius = 20
         this.direction = 'down'
         this.speed = 1
+        this.previousPosx = this.pos.x
+        this.previousPosy = this.pos.y
     }
     draw(){
         ctx.beginPath()
@@ -56,6 +58,8 @@ class Player{
         ctx.closePath()
     }
     move(){
+        this.previousPosx = this.pos.x
+        this.previousPosy = this.pos.y
         if(this.direction == "up"){
             this.pos.y -= this.speed
         }else if(this.direction == "down"){
@@ -64,6 +68,24 @@ class Player{
             this.pos.x -= this.speed
         }else if(this.direction == "right"){
             this.pos.x += this.speed
+        }
+        for(let i = 0; i < boundaries.length; i++)
+        {
+            if (isTouching(this.pos.x + this.radius, this.pos.x - this.radius, boundaries[i].pos.x, boundaries[i].pos.x + blockSize, 
+                           this.pos.y + this.radius, this.pos.y - this.radius, boundaries[i].pos.y, boundaries[i].pos.y + blockSize)){
+                console.log(143214)
+                this.pos.x = this.previousPosx
+                this.pos.y = this.previousPosy
+            }
+        }
+
+        for(let i = 0; i < foods.length; i++)
+        {
+            if (isTouching(this.pos.x + this.radius, this.pos.x - this.radius, foods[i].pos.x - this.radius, foods[i].pos.x + this.radius, 
+                           this.pos.y + this.radius, this.pos.y - this.radius, foods[i].pos.y - this.radius, foods[i].pos.y + this.radius)){
+                console.log(515)
+                foods.splice(i,1)
+            }
         }
     }
 }
@@ -81,7 +103,27 @@ class Boundary{
         ctx.drawImage(this.img, this.pos.x, this.pos.y, this.blockSize, this.blockSize)
     }
 }
-const player = new Player({x:10, y:10})
+class Food{
+    constructor(pos){
+        this.pos = pos
+        this.radius = 5
+    }
+    draw(){
+        ctx.beginPath()
+        ctx.fillStyle = 'yellow'
+        ctx.arc(this.pos.x, this.pos.y, this.radius, 
+            0, Math.PI * 2)
+        ctx.stroke()
+        ctx.fill()
+        ctx.closePath()
+    }
+}
+function isTouching(x1, x2, x3, x4, y1, y2, y3, y4){
+    return x1 >= x3 && x2 <= x4 && y1 >= y3 && y2 <= y4
+}
+
+
+const player = new Player({x:blockSize * 1.5, y:blockSize * 1.5})
 document.body.addEventListener('keydown', (e) => {
     if(e.key == 'w'){
         player.direction = 'up'
@@ -97,11 +139,14 @@ document.body.addEventListener('keydown', (e) => {
 function draw(){
     ctx.fillStyle = '#000000'
     ctx.fillRect(0, 0, innerWidth, innerHeight)
-    player.draw()
-    player.move()
     for(let i = 0; i < boundaries.length; i++){
         boundaries[i].draw()
     }
+    for(let i = 0; i < foods.length; i++){
+        foods[i].draw()
+    }
+    player.draw()
+    player.move()
 }
 
 for(let i = 0; i < map.length; i++){
@@ -111,6 +156,10 @@ for(let i = 0; i < map.length; i++){
                 {x:startPosX + blockSize * j, y: startPosY + blockSize * i},
                 textToImg[map[i][j]],
                 blockSize
+            ))
+        }else{
+            foods.push(new Food(
+                {x:startPosX + blockSize * j + blockSize / 2, y: startPosY + blockSize * i + blockSize / 2},
             ))
         }
     }
